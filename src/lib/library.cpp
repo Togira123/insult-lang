66ec090bdf99a9bf0da84f0754f18d186ba2491c
Togira <70365614+Togira123@users.noreply.github.com>
2023-08-09 12:19:56 +0200

    added readline library function

--- src/lib/library.cpp
+++ src/lib/library.cpp
@@ -3,6 +3,7 @@
 
 identifier_detail& identifier_detail_of(intermediate_representation& ir, const std::string& id_name) {
     if (id_name == "print") {
+        // print a value to stdout
         static const std::vector<types> ts = {types::BOOL_TYPE, types::DOUBLE_TYPE, types::INT_TYPE, types::STRING_TYPE};
         static bool has_been_added_to_ir = false;
         static std::vector<size_t> fi(ts.size());
@@ -17,7 +18,20 @@ identifier_detail& identifier_detail_of(intermediate_representation& ir, const s
         }
         static identifier_detail id = {{types::FUNCTION_TYPE, types::UNKNOWN_TYPE, 0, fi}, -1, true, true, true};
         return id;
+    } else if (id_name == "readline") {
+        // reads a line from stdin
+        static bool has_been_added_to_ir = false;
+        static std::vector<size_t> fi(1);
+        if (!has_been_added_to_ir) {
+            auto* cur_scope = ir.scopes.new_scope();
+            fi[0] = ir.function_info.size();
+            ir.function_info.push_back({{types::STRING_TYPE}, {}, true, cur_scope});
+            has_been_added_to_ir = true;
+        }
+        static identifier_detail id = {{types::FUNCTION_TYPE, types::UNKNOWN_TYPE, 0, fi}, -1, true, true, true};
+        return id;
     } else if (id_name == "size") {
+        // obtain the size of a container
         static const std::vector<full_type> ts = {{types::STRING_TYPE}, {types::ARRAY_TYPE, types::UNKNOWN_TYPE, -1}};
         static bool has_been_added_to_ir = false;
         static std::vector<size_t> fi(ts.size());
@@ -33,6 +47,7 @@ identifier_detail& identifier_detail_of(intermediate_representation& ir, const s
         static identifier_detail id = {{types::FUNCTION_TYPE, types::UNKNOWN_TYPE, 0, fi}, -1, true, true, true};
         return id;
     } else if (id_name == "copy") {
+        // return a copy of the given array instead of passing by reference
         static const std::vector<full_type> ts = {
             {types::STRING_TYPE},
         };
@@ -48,6 +63,7 @@ identifier_detail& identifier_detail_of(intermediate_representation& ir, const s
         static identifier_detail id = {{types::FUNCTION_TYPE, types::UNKNOWN_TYPE, 0, fi}, -1, true, true, true};
         return id;
     } else if (id_name == "ref") {
+        // return a reference to the given value instead of a copy
         static const std::vector<types> ts = {types::INT_TYPE, types::DOUBLE_TYPE, types::BOOL_TYPE, types::STRING_TYPE};
         static bool has_been_added_to_ir = false;
         static std::vector<size_t> fi(ts.size());
